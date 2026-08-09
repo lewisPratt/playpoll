ebc01d70902f420c3b1f7eda0721366b253fb92a
spikereloaded <lewis.198804@gmail.com>
2026-08-09 23:56:29 +0100

    Milestone - Search logic and functionality working and returning game results to console.  update searchGame to accept searchTerm parameter

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -7,6 +7,8 @@ interface SearchBoxProps {
 
 function SearchBox({ searchBoxSetter, slotIdentifier }: SearchBoxProps) {
   let timer: number;
+  const publishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+
   function closeOverlay(e: React.MouseEvent<HTMLDivElement>) {
     const clicked = e.target;
     if (clicked === e.currentTarget) {
@@ -17,12 +19,41 @@ function SearchBox({ searchBoxSetter, slotIdentifier }: SearchBoxProps) {
   function debounceSearch(e: React.ChangeEvent<HTMLInputElement>) {
     if (e.target.value != "") {
       clearTimeout(timer);
-      timer = setTimeout(searchGame, 2000);
+      timer = setTimeout(function () {
+        searchGame(e.target.value);
+      }, 2000);
     }
   }
 
-  async function searchGame() {
-    alert("search sent");
+  async function searchGame(searchTerm: string) {
+    console.log(searchTerm);
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://lmzsnthuaysxrqgygfwm.supabase.co/functions/v1/quick-api`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            searchTerm: `${searchTerm}`,
+          }),
+          headers: {
+            Authorization: `Bearer ${publishableKey}`,
+            accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        },
+      );
+      const data = await response.json();
+      if (data.error) {
+        console.log("error");
+        //setErrorMsg(data.error.error);
+        console.log(data);
+      } else {
+        //successful api call
+        console.log(data);
+      }
+    };
+
+    fetchData();
   }
   return (
     <>
@@ -41,6 +72,7 @@ function SearchBox({ searchBoxSetter, slotIdentifier }: SearchBoxProps) {
               name="game-slot"
               hidden
               value={slotIdentifier}
+              readOnly
             />
           </form>
         </div>
